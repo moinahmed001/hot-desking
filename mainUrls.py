5911ab83f2fef08d8cfae00c89354a34931b0b2e
Ahmed, Moin <Moin.Ahmed@sky.uk>
2019-11-01 16:28:24 +0000

    aa

--- mainUrls.py
+++ mainUrls.py
@@ -40,8 +40,30 @@ def all_available_desks():
 @app.route("/all-desks")
 def all_desks():
     all_desks = get_all_desks()
-    return render_template('all-desks.html', all_desks=all_desks)
+    desks_layed_out = all_desks_layed_out()
+    print(desks_layed_out)
+    return render_template('all-desks.html', all_desks=all_desks, desks_layed_out=desks_layed_out)
 
+
+def all_desks_layed_out():
+    # layout = [4, 6, 6, 6, 6, 6, 6, 6]
+    all_desks = get_all_desks()
+
+    return [{'one': [fetch_desk(all_desks, 70, 66), fetch_desk(all_desks, 74, 70)]},
+    {'two': [fetch_desk(all_desks, 80, 74), fetch_desk(all_desks, 86, 80)]},
+    {'three': [fetch_desk(all_desks, 92, 86), fetch_desk(all_desks, 98, 92)]},
+    {'four': [fetch_desk(all_desks, 104, 98), fetch_desk(all_desks, 110, 104)]},
+    {'six': [fetch_desk(all_desks, 116, 110), fetch_desk(all_desks, 122, 116)]},
+    {'seven': [fetch_desk(all_desks, 128, 122), fetch_desk(all_desks, 134, 128)]}
+     ]
+
+def fetch_desk(all_desks, start, end_number):
+    desks = []
+    for i in range(start, end_number, -1):
+        for desk in all_desks:
+            if(desk['desk_number'] == i):
+                desks.append(desk)
+    return desks
 
 if __name__ == "__main__":
     app.run(debug=True)
